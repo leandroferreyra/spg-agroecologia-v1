82fc8e3bcb68e461cf3af41ae9e8cfed30e68404
Leandro Ferreyra <lean224@hotmail.com>
2025-01-16 14:24:52 -0300

    nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,33 @@ import { Subscription } from 'rxjs';
 
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-menu-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
+
+  menuItems = [
+    {
+      label: 'Productos',
+      subMenu: [
+        { label: 'Nuevo producto', route: '/productos/nuevo' },
+        { label: 'Listado de productos', route: '/productos/listado' }
+      ]
+    },
+    {
+      label: 'Proveedores',
+      subMenu: [
+        { label: 'Nuevo proveedor', route: '/proveedores/nuevo' },
+        { label: 'Listado de proveedores', route: '/proveedores/listado' }
+      ]
+    },
+    { label: 'Clientes', subMenu: [] },
+    { label: 'Configuración', subMenu: [] },
+    { label: 'Salir', subMenu: [] }
+  ];
 
   private subscription: Subscription = new Subscription();
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopNavbarComponent } from '../components/top-navbar/top-navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, TopNavbarComponent,NavbarComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
