40bf367dba559c6f29e0160b736b27434d3966df
Leandro Ferreyra <lean224@hotmail.com>
2025-01-20 12:41:56 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-
-    {
-        path: '',
-        loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
-    },
-    {
-        path: 'auth',
-        loadChildren: () => import('../app/auth/auth.routes').then(m => m.AUTH_ROUTES)
-    },
-
-];
+export const routes: Routes = [];
