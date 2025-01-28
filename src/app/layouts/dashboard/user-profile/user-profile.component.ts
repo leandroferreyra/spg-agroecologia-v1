c2b34550a0e15a46b81ab7b84b9d771fe24c2e08
Leandro Ferreyra <lean224@hotmail.com>
2025-01-28 17:33:00 -0300

    perfil de usuario

--- src/app/layouts/dashboard/user-profile/user-profile.component.ts
+++ src/app/layouts/dashboard/user-profile/user-profile.component.ts
@@ -1,26 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { UserLoggedService } from 'src/app/core/services/user-logged.service';
+import { SharedModule } from 'src/shared.module';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription = new Subscription();
+  usuarioLogueado: any;
 
+  isEdicion = false;
 
-  constructor() {
+  constructor(private _userLogged: UserLoggedService) {
 
   }
 
   ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {
+    this.usuarioLogueado = this._userLogged.getUsuarioLogueado;
+    console.log(this.usuarioLogueado);
   }
 
 
