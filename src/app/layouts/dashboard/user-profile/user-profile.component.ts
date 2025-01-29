9f7c8399496519e78243944233d4f2768f6963aa
Leandro Ferreyra <lean224@hotmail.com>
2025-01-29 15:47:32 -0300

    user-profile

--- src/app/layouts/dashboard/user-profile/user-profile.component.ts
+++ src/app/layouts/dashboard/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { UserLoggedService } from 'src/app/core/services/user-logged.service';
 import { SharedModule } from 'src/shared.module';
@@ -16,7 +17,9 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
   usuarioLogueado: any;
 
-  isEdicion = false;
+  isEdicion: boolean = false;
+  userForm!: FormGroup;
+
 
   constructor(private _userLogged: UserLoggedService) {
 
@@ -29,6 +32,43 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.usuarioLogueado = this._userLogged.getUsuarioLogueado;
     console.log(this.usuarioLogueado);
+    this.inicializarForm();
+  }
+
+  inicializarForm() {
+    this.userForm = new FormGroup({
+      email: new FormControl({ value: this.usuarioLogueado.email, disabled: true }, [Validators.required, Validators.email]),
+      usuario: new FormControl({ value: this.usuarioLogueado.user_name, disabled: true }, [Validators.required, Validators.email]),
+      nombres: new FormControl({ value: this.usuarioLogueado.firstname, disabled: true }, [Validators.required]),
+      apellidos: new FormControl({ value: this.usuarioLogueado.lastname, disabled: true }, [Validators.required]),
+      genero: new FormControl({ value: '', disabled: true }, [Validators.required]),
+      ciudad: new FormControl({ value: '', disabled: true }, [Validators.required]),
+      direccion: new FormControl({ value: '', disabled: true }, [Validators.required]),
+    });
+    this.onFormChange();
+  }
+
+  onFormChange() {
+
+  }
+
+  toggleEdicion() {
+    this.isEdicion = !this.isEdicion;
+    if (this.isEdicion) {
+      this.modificarValidacionesForm();
+    } else {
+      this.inicializarForm();
+    }
+  }
+
+  modificarValidacionesForm() {
+    this.userForm.get('email')?.enable();
+    this.userForm.get('usuario')?.enable();
+    this.userForm.get('nombres')?.enable();
+    this.userForm.get('apellidos')?.enable();
+    this.userForm.get('genero')?.enable();
+    this.userForm.get('ciudad')?.enable();
+    this.userForm.get('direccion')?.enable();
   }
 
 
