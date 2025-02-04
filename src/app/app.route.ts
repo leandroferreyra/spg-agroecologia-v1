66271d9a90a81f1e84228b79c3662b0701d24bf7
Leandro Ferreyra <lean224@hotmail.com>
2025-02-04 10:37:18 -0300

    login guard

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -10,10 +10,10 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
-        canActivate: [LoginGuard]
     },
     {
         path: 'dashboard',
         loadChildren: () => import('./layouts/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+        canActivate: [LoginGuard]
     }
 ];
--- src/app/core/guards/loginGuard.ts
+++ src/app/core/guards/loginGuard.ts
@@ -7,10 +7,11 @@ export const LoginGuard = () => {
 
     if (localStorage.getItem('token')) {
         console.log('[LoginGuard] Token encontrado, redirigiendo a dashboard');
-        router.navigate(['/dashboard']);
-        return false;
+        // router.navigate(['/dashboard']);
+        return true;
     } else {
         console.log('[LoginGuard] Token no encontrado, permitiendo acceso a login');
-        return true;
+        router.navigate(['auth/boxed-signin']);
+        return false;
     }
 }
