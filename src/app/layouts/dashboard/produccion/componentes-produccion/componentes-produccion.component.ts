55a88f272c55db91bfd235c834328958fa5b1848
Leandro Ferreyra <lean224@hotmail.com>
2025-08-25 18:24:39 -0300

    fix

--- src/app/layouts/dashboard/produccion/componentes-produccion/componentes-produccion.component.ts
+++ src/app/layouts/dashboard/produccion/componentes-produccion/componentes-produccion.component.ts
@@ -84,10 +84,6 @@ export class ComponentesProduccionComponent implements OnInit, OnDestroy {
 
   serialesPorLote: Record<string, any[]> = {};
 
-  expandirTodo = false;
-  componentesExpandido: { [uuid: string]: boolean } = {}; // Estado de expansión de cada compra
-
-
   constructor(private spinner: NgxSpinnerService, private _indexService: IndexService, private _tokenService: TokenService,
     private _swalService: SwalService, private _frozenComponentService: FrozenComponentService, private router: Router) {
 
@@ -284,7 +280,7 @@ export class ComponentesProduccionComponent implements OnInit, OnDestroy {
   // }
 
   openModalComponente(data: any) {
-    // this.toggleComponente(data);
+    this.selectedComponent = data;
     this.inicializarFormComponente(data);
     this.obtenerProveedoresByComponente(data);
     this.stocks = (data.possible_stocks || []).map((stock: any) => ({
@@ -294,6 +290,7 @@ export class ComponentesProduccionComponent implements OnInit, OnDestroy {
     this.tituloModal = `Selección de origen de "${data.name}"`;
     this.modalComponente.options = this.modalOptions;
     this.modalComponente.open();
+
   }
 
   cerrarModal() {
@@ -579,16 +576,5 @@ export class ComponentesProduccionComponent implements OnInit, OnDestroy {
 
   toggleTodos() {
     this.ocultarSinStock = !this.ocultarSinStock;
-  }
-
-  toggleComponente(data: any) {
-    this.componentesExpandido[data.uuid] = !this.componentesExpandido[data.uuid];
-  }
-
-  toggleTodosExpandidos() {
-    this.expandirTodo = !this.expandirTodo;
-    this.componentes.forEach(compra => {
-      this.componentesExpandido[compra.uuid] = this.expandirTodo;
-    });
   }
 }
