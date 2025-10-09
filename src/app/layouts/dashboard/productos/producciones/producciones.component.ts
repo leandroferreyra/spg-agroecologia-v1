4ffe4ba4c616b5198f4ec9b72e5f886f3c7a2575
Leandro Ferreyra <lean224@hotmail.com>
2025-10-09 13:37:11 -0300

    producciones de producto

--- src/app/layouts/dashboard/productos/producciones/producciones.component.ts
+++ src/app/layouts/dashboard/productos/producciones/producciones.component.ts
@@ -1,12 +1,123 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { NgxTippyModule } from 'ngx-tippy-wrapper';
+import { Subscription, firstValueFrom, map, catchError, of } from 'rxjs';
+import { IndexService } from 'src/app/core/services/index.service';
+import { SwalService } from 'src/app/core/services/swal.service';
+import { TiposCambioService } from 'src/app/core/services/tiposCambio.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { IconArrowForwardComponent } from 'src/app/shared/icon/icon-arrow-forward';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-producciones',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxTippyModule, NgSelectModule, NgbPaginationModule, NgxSpinnerModule, IconArrowForwardComponent],
   templateUrl: './producciones.component.html',
   styleUrl: './producciones.component.css'
 })
-export class ProduccionesComponent {
+export class ProduccionesComponent implements OnInit, OnDestroy {
+
+  @Input() producto: any;
+  @Input() rol!: string;
+  producciones: any[] = [];
+
+  private subscription: Subscription = new Subscription();
+
+  // Orden, filtro y paginación para compras de proveedor
+  MAX_ITEMS_PER_PAGE = 10;
+  currentPage = 1;
+  last_page = 1;
+  itemsPerPage = this.MAX_ITEMS_PER_PAGE;
+  itemsInPage = this.itemsPerPage;
+  pageSize: number = 0;
+  total_rows: number = 0;
+
+  filtros: any = {
+    'product.uuid': { value: '', op: '=', contiene: false },
+  };
+  ordenamiento: any = {
+  };
+
+  constructor(private _indexService: IndexService, private _swalService: SwalService, private spinner: NgxSpinnerService,
+    private _tokenService: TokenService, private router: Router, private _tiposCambioService: TiposCambioService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['producto'] && changes['producto'].currentValue) {
+      this.spinner.show();
+      // Si el producto cambia, actualizamos los filtros y obtenemos los componentes
+      this.filtros['product.uuid'].value = this.producto.uuid;
+      this.obtenerProducciones();
+    }
+  }
+
+  obtenerProducciones() {
+    // Inicializamos un objeto vacío para los parámetros
+    const params: any = {};
+    params.with = ["batch", "responsible", "currentState"];
+    params.paging = this.itemsPerPage;
+    params.page = this.currentPage;
+    params.order_by = this.ordenamiento;
+    params.filters = this.filtros;
+
+    this.subscription.add(
+      this._indexService.getProduccionesWithParam(params, this.rol).subscribe({
+        next: res => {
+          this.producciones = res.data;
+          this.modificarPaginacion(res);
+          this._tokenService.setToken(res.token);
+          this.spinner.hide();
+        },
+        error: error => {
+          this._swalService.toastError('top-right', error.error.message);
+          console.error(error);
+          this.spinner.hide();
+        }
+      })
+    )
+  }
+
+  modificarPaginacion(res: any) {
+    this.total_rows = res.meta.total;
+    this.last_page = res.meta.last_page;
+    if (this.producciones.length <= this.itemsPerPage) {
+      if (res.meta?.current_page === res.meta?.last_page) {
+        this.itemsInPage = this.total_rows;
+      } else {
+        this.itemsInPage = this.currentPage * this.itemsPerPage;
+      }
+    }
+  }
+
+  irALaProduccion(event: MouseEvent, data: any) {
+    const baseUrl = window.location.origin + window.location.pathname;
+    const urlTree = this.router.createUrlTree([`/dashboard/producciones/${data.uuid}`]);
+    const url = this.router.serializeUrl(urlTree);
+    if (event.ctrlKey || event.metaKey) {
+      window.open(`${baseUrl}#${url}`, '_blank');
+    } else {
+      this.router.navigate([`/dashboard/producciones/${data.uuid}`]);
+    }
+  }
+
+  convertirFechaConHora(fechaStr: string): string {
+    if (!fechaStr) return '';
+    const [fecha, hora] = fechaStr.split(' ');
+    const [anio, mes, dia] = fecha.split('-');
+    return `${dia}-${mes}-${anio} ${hora}`;
+  }
 
 }
