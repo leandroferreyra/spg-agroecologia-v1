c730ababac84d5ebf14bf15540e427c088a54742
Leandro Ferreyra <lean224@hotmail.com>
2026-05-09 20:30:23 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // baseUrl: 'http://localhost/ladie-api/public/api'
-    baseUrl: 'https://ladie-api.dataexpertise.com.ar/public/api'
+    baseUrl: 'https://spg-agroecologia-backend.onrender.com/tesina'
 };
